8f11f922aec9e00c70306ca952b7035a64762db8
Alex Parella <apar20@bu.edu>
2019-11-07 19:24:54 -0500

    hey

---
+++ v1.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
